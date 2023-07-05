4b3ebbc40dcce3a125d0a3efe116ac1488b260f6
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-07-05 16:21:14 +0800

    Update for 3.2

--- index.js
+++ index.js
@@ -4,30 +4,38 @@ const app = express()
 app.use(express.json())
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
 ]
 
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
+app.get('/api/persons', (_, response) => {
+  response.json(persons)
+})
+
+app.get('/info', (_, response) => {
+  const body = `
+    <p>Phone book has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+  `
+  response.send(body)
 })
 
 const PORT = 3001
