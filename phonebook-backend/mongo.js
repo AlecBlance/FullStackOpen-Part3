5ff421f1c69c1b47d3ba517cafbc5d84dd322e8e
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-07-12 13:00:28 +0800

    Add 3.12 changes

---
+++ phonebook-backend/mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstackopen:${password}@fullstackopen.wto0f0n.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const entrySchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Entry = mongoose.model('Entry', entrySchema)
+
+
+if (process.argv.length > 3) {
+  const name = process.argv[3]
+  const number = process.argv[4]
+  const entry = Entry({
+    name: name,
+    number: number
+  })
+  entry.save().then(_ => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+} else {
+  Entry.find({}).then(result => {
+    console.log('phonebook:')
+    result.map(({name, number}) => console.log(`${name} ${number}`))
+    mongoose.connection.close()
+  })
+}
+
+
+
+
