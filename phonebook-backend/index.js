303a59fbcc9d30c4b34c1cfad5e806b5195a7006
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-07-13 11:31:47 +0800

    Update for 3.16-3.17

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -6,6 +6,17 @@ const Entry = require('./models/entry')
 
 morgan.token('body', (req, _) => JSON.stringify(req.body))
 
+const errorHandler = (error, req, res, next) => {
+  if (error.name === 'NameError') return res.status(400).json({error: "name required"})
+  if (error.name === 'NumberError') return res.status(400).json({error: "phone number required"})
+  if (error.name === 'CastError') return res.status(400).json({error: "malformed id"})
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
 app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -57,21 +68,36 @@ app.get('/api/persons/:id', (req, res)=>{
 })
 
 // rest: delete_phonebook_entry
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   Entry.findByIdAndRemove(req.params.id)
     .then(_ => res.status(204).end())
+    .catch(error => next(error))
 })
 
 // rest: add_phonebook_entry
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const person = req.body
   const {name, number} = person
-  if (!name) return res.status(400).json({error: "name required"})
-  if (!number) return res.status(400).json({error: "phone number required"})
+  if (!name) return next({name: 'NameError'})
+  if (!number) return next({name: 'NumberError'})
   Entry(person)
     .save()
     .then(person => res.json(person))
 })
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const entryInfo = {
+    name: req.body.name,
+    number: req.body.number
+  }
+  console.log(entryInfo)
+  Entry.findByIdAndUpdate(req.params.id, entryInfo,{new: true})
+    .then(entry => res.json(entry))
+    .catch(error => next(error))
+})
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
